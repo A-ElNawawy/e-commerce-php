a376229690a29898b0d3012f262049684947f176
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-11-01 09:25:23 +0200

    comment on getTitle function

--- admin/includes/functions/functions.php
+++ admin/includes/functions/functions.php
@@ -1,4 +1,8 @@
 <?php
+/*
+** Title Function That Echo The Page Title In Case The Page
+** Has The Variable $pageTitle And Echo Default Title For 
+*/
   function getTitle(){
     global $pageTitle;
     if(isset($pageTitle)){
