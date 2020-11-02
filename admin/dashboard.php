632dd00ace2933a69ab98fc4013dc669fa1b6fc1
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-11-02 10:49:08 +0200

    Create Dashboard Layout

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,9 +3,66 @@
   
   if(isset($_SESSION['username'])){
     $pageTitle = 'Dashboard';
-
     include 'init.php';
-    echo '<h1 class="text-center">Welcome in Dashboard</h1>';
+    //=======================================================
+?>
+    <!--  -->
+    <div class="container home-stats text-center">
+      <h1>Dashboard</h1>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="stat">
+            Total Members
+            <span>200</span>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="stat">
+            Pending Members
+            <span>25</span>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="stat">
+            Total Items
+            <span>1500</span>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="stat">
+            Total Comments
+            <span>12000</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container latest">
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-header">
+              <i class="fa fa-users"></i> Latest Registered Users
+            </div>
+            <div class="card-body">
+              Test
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-header">
+              <i class="fa fa-users"></i> Latest Items
+            </div>
+            <div class="card-body">
+              Test
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+<?php
+    //=======================================================
     include $templates . 'footer.php';
   }else{
     header('location: index.php');
