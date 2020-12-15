eba3faa161cc727f4f2cf7078ea9149daadb3896
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-12-15 18:25:33 +0200

    frontend => create categories page

---
+++ categories.php
@@ -0,0 +1,49 @@
+<?php
+  $pageTitle = 'Main';
+
+  /* start includes */
+  include './init.php';
+  /* end includes */
+  //=======================================================
+
+  $catid = isset($_GET['catid'])? $_GET['catid'] : 0;
+  $catname = isset($_GET['catname'])? $_GET['catname'] : "All";
+  $items = getItems($catid);
+  ?>
+    <div class="container categories">
+      <h1 class="text-center"><?php echo $catname ?></h1>
+      <div class="items-holder">
+        <?php
+          if(!empty($items)){
+            foreach($items as $item){
+              echo '
+                <div border class="Card box2">
+                  <span class="price">'. $item['Price'] .'</span>
+                  <div class="image">
+                    <img
+                      src="./data/images/productsImages/download.jfif"
+                      alt="Product"
+                    />
+                  </div>
+                  <div class="content">
+                    <h5><a href="#">'. $item['Name'] .'</a></h5>
+                    <p class="description">'. $item['Description'] .'</p>
+                    <div class="read-more">
+                      <button>
+                        <a href="#">Read more</a>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ';
+            }
+          }else{
+            echo '<div class="alert alert-info no-item-message">There Is No Members</div>';
+          }
+        ?>
+      </div>
+    </div>
+  <?php
+
+  //=======================================================
+  include $templates . 'footer.php';
--- index.php
+++ index.php
@@ -4,30 +4,13 @@
   /* start includes */
   include './init.php';
   /* end includes */
-      $stmt = $con->prepare("SELECT * FROM categories");
-      // Execute The Statement
-      $stmt ->execute();
-      // Assign To Variable
-      $rows = $stmt->fetchAll();
-      //$rows = [];
-      if(empty($rows)) {
-    ?>
-      <div class="container">
-        <div class="alert alert-info no-item-message">There Is No Categories</div>
-      </div>
-    <?php
-      }else{
-    ?>
-      <div class="container">
-        <h1>Categories</h1>
-        <?php
-          foreach($rows as $row){
-            echo '<div>';
-              echo $row['Name'];
-            echo '</div>';
-          }
-        ?>
-      </div>
-    <?php
-      }
+  //=======================================================
+
+  ?>
+    <div class="container">
+      <h1>Home</h1>
+    </div>
+  <?php
+
+  //=======================================================
   include $templates . 'footer.php';
